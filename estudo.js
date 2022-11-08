9dbee6e262fd6123dc0af7dd3646117c80c2474b
yujiTMZ <106184172+yujiTMZ@users.noreply.github.com>
2022-11-08 15:43:46 -0300

    8/11

--- estudo.js
+++ estudo.js
@@ -63,12 +63,14 @@ function total(){
       return 
    }
    let r = val;
+   let texto = "";
    for(let m = 1; m <= t; m++){
       r = (val * (1+ (ju/100)));
       val = r;
+      texto +=  m + ": " + moeda(r) + "<br>"
       //document.write("Mês " + m + " valor: " + moeda(r) + "<br>");
    }
-   
+      document.getElementById("ListaMes").innerHTML = texto;
       document.getElementById("total").innerHTML = "Total: "+ moeda (r);
    //document.write("O tatal é " + moeda(r));
 }
